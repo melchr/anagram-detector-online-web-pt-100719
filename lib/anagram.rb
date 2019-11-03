aeec2bcd949abefc77f6938d74eb82f0ecc28fd1
Melissa Christman <melchrstmn@gmail.com>
2019-11-03 20:37:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(anagrams)
+    anagrams.select {|w| w.split("").sort == @name.split("").sort}
+  end
+end
